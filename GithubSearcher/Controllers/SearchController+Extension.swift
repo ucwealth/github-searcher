80e6bcbca81f25578c0b0a56f8a31057158991dc
ucwealth <ucheokeke16@gmail.com>
2023-02-03 13:30:07 +0100

    add gitignore and extensions file

---
+++ GithubSearcher/Controllers/SearchController+Extension.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+// MARK: - NetworkManagerDelegate
+
+extension SearchController: NetworkManagerDelegate {
+    
+    func didUpdateUser(_ networkManager: NetworkManager, user:  User) {
+        DispatchQueue.main.async {[weak self] in
+            self?.userList.append(contentsOf: user.items)
+            self?.tableView.reloadData()
+            self?.tableView.isHidden = false
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print("Error: ",error)
+    }
+    
+//    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+//        DispatchQueue.main.async {
+//            self.temperatureLabel.text = weather.tempString
+//            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+//            self.cityLabel.text = weather.cityName
+//        }
+//    }
+    
+}
+
+// MARK: - TextFieldDelegate
+
+extension SearchController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return true
+    }
+
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let input = searchTextField.text {
+            networkManager.fetchUser(searchQuery: input)
+        }
+        textField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    
+}
+
--- GithubSearcher/Controllers/SearchController.swift
+++ GithubSearcher/Controllers/SearchController.swift
@@ -3,10 +3,11 @@ import UIKit
 class SearchController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchTextField: UITextField!
-
+    
     var resultViewModel = ResultViewModel()
     var networkManager = NetworkManager()
-    var user: User?
+    var userList = [Item]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Search github users"
@@ -19,38 +20,33 @@ class SearchController: UIViewController {
                     forCellReuseIdentifier: ResultCell.identifier)
         tableView.isHidden = true
     }
-
 }
-
 // pagination
 // style table row properly
 // sort alphabetically by login
-
-extension SearchController: UITableViewDataSource, UITableViewDelegate {
+// add sdwebimage
+// 
+extension SearchController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return user?.totalCount ?? 10
-        let resultArray = user?.items
-        return resultArray?.count ?? 1
+        userList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ResultCell.identifier, for: indexPath) as? ResultCell else {
             return UITableViewCell()
         }
-        let resultArray = user?.items
-        if let result = resultArray?[indexPath.row] {
-            cell.configure(result)
-        }
+        cell.configure(userList[indexPath.row])
         return cell
     }
-    
+}
+
+extension SearchController: UITableViewDelegate, UIScrollViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
         let detailsVC = DetailViewController()
-        var resultArray = user?.items
-        detailsVC.result = resultArray?[indexPath.row]
+        detailsVC.result = userList[indexPath.row]
         navigationController?.pushViewController(detailsVC, animated: true)
 
 //        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailsController {
@@ -60,58 +56,14 @@ extension SearchController: UITableViewDataSource, UITableViewDelegate {
         
     }
     
-}
-// MARK: - NetworkManagerDelegate
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let position = scrollView.contentOffset.y
+        if position > (tableView.contentSize.height-100-scrollView.frame.size.height) {
+            // fetch more data
 
-extension SearchController: NetworkManagerDelegate {
-    
-    func didUpdateUser(_ networkManager: NetworkManager, user:  User) {
-        DispatchQueue.main.async {[weak self] in
-            self?.user = user
-            self?.tableView.reloadData()
-            self?.tableView.isHidden = false
         }
-    }
-    
-    func didFailWithError(error: Error) {
-        print("Error: ",error)
-    }
-    
-//    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
-//        DispatchQueue.main.async {
-//            self.temperatureLabel.text = weather.tempString
-//            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
-//            self.cityLabel.text = weather.cityName
+//        if position > scrollView.frame.size.height {
+//            print("fetch pageinate")
 //        }
-//    }
-    
-}
-// MARK: - TextFieldDelegate
-
-extension SearchController: UITextFieldDelegate {
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.endEditing(true)
-        return true
-    }
-
-    @IBAction func searchButtonPressed(_ sender: UIButton) {
-        searchTextField.endEditing(true)
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let input = searchTextField.text {
-            networkManager.fetchUser(searchQuery: input)
-        }
-        textField.text = ""
-    }
-    
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        if textField.text != "" {
-            return true
-        } else {
-            textField.placeholder = "Type Something"
-            return false
-        }
     }
 }
